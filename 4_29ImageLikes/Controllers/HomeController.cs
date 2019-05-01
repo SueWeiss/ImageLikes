f5b82a7064dcb4ccecf9da331b0f4b5d43b80455
Sarah Weiss <suriw22@gmail.com>
2019-05-01 18:03:53 -0400

    Commit

--- 4_29ImageLikes/Controllers/HomeController.cs
+++ 4_29ImageLikes/Controllers/HomeController.cs
@@ -5,29 +5,72 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using _4_29ImageLikes.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Library.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
 
 namespace _4_29ImageLikes.Controllers
 {
     public class HomeController : Controller
     {
+        private string _connectionString;
+        private IHostingEnvironment _environment;
+        public HomeController(IConfiguration configuration,IHostingEnvironment environment)
+        {
+            _environment = environment;
+            _connectionString = configuration.GetConnectionString("ConStr");
+        }
         public IActionResult Index()
         {
-            return View();
+            Manager mgr = new Manager(_connectionString);
+               return View(mgr.GetImages());
         }
 
-        public IActionResult About()
+        public IActionResult Upload()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
-
-        public IActionResult Contact()
+        public IActionResult AddImage(IFormFile imageName,string title)
         {
-            ViewData["Message"] = "Your contact page.";
+            string fileName = $"{Guid.NewGuid()}{Path.GetExtension(imageName.FileName)}";
+            string fullPath = Path.Combine(_environment.WebRootPath, "My_Images", fileName);
+            using (FileStream stream = new FileStream(fullPath, FileMode.CreateNew))
+            {
+                imageName.CopyTo(stream);
+            }
+            Image i = new Image { ImageName = fileName, Title = title, Likes = 0 ,DateAdded=DateTime.Now};
+           // i.ImageName = fileName; //fileName
 
-            return View();
+            Manager mgr = new Manager(_connectionString);
+             mgr.InsertImage(i);
+            return Redirect("/");
         }
+        public IActionResult Image(int id)
+        {
+            Manager mgr = new Manager(_connectionString);
+            var i=  mgr.GetImageById(id);
+            return View(i);
+        }
+        [HttpPost]
+        public IActionResult addLike(int Id)
+        {
+            Manager mgr = new Manager(_connectionString);
+            mgr.SetLike(Id);
+            return Json(Id);
+        }
+        [HttpPost]
+        public IActionResult GetLikes(int id)
+        {
+            Manager mgr = new Manager(_connectionString);
+            int likes=mgr.GetLike(id);
+            return Json(likes);
+        }
+
+
+
+
 
         public IActionResult Privacy()
         {
